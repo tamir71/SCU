690e38af344af2545887a895524f9f0a2c9840df
Tamir Enkhjargal <tamir71@gmail.com>
2019-10-01 21:33:14 -0700

    Added README for COEN20 Course

--- COEN/COEN20/Lab1/bitsigns.c
+++ COEN/COEN20/Lab1/bitsigns.c
@@ -21,10 +21,10 @@ void        Unsigned2Bits(uint32_t n, int8_t bits[8]);	// Opposite of Bits2Unsig
 
 int32_t Bits2Signed(int8_t bits[8]) {
     int32_t retValue = Bits2Unsigned(bits);				// Call Bits2Unsigned which gives you a value from 0 to 255
-    if(retValue > 127) {								// and then adjust for rollover and the range becomes -128 to +127
+    if(retValue > 127) {						// and then adjust for rollover and the range becomes -128 to +127
 		retValue -= 256;
 	}
-	return retValue;									// Return our signed value
+	return retValue;						// Return our signed value
 }
 
 /*-------------------------*/
@@ -32,14 +32,14 @@ int32_t Bits2Signed(int8_t bits[8]) {
 /* Bits to Unsigned Function */
 
 uint32_t Bits2Unsigned(int8_t bits[8]) {
-    uint32_t retValue = 0;								// Create retValue variable
+    uint32_t retValue = 0;						// Create retValue variable
     int i;
 
-    for(i = 7; i >= 0; i--) {							// Using the code from the hint, create a loop to essentially
+    for(i = 7; i >= 0; i--) {						// Using the code from the hint, create a loop to essentially
         retValue = 2*retValue + bits[i];				// determine the unsigned value using "polynomial expansion"
     }
     
-    return retValue;									// Return our unsigned value
+    return retValue;						        // Return our unsigned value
 }
 
 /*-------------------------*/
@@ -48,16 +48,16 @@ uint32_t Bits2Unsigned(int8_t bits[8]) {
 
 void Increment(int8_t bits[8]) {
     int i;
-    for(i = 0; i < 8; i++) {							// Loop through our whole bit array, if necessary
-        if(bits[i] == 0) {								// If it hits a 0, increment to 1
-            bits[i] = 1;								// "Increment"
-            break;										// Exit the loop once we know we've incremented
+    for(i = 0; i < 8; i++) {						// Loop through our whole bit array, if necessary
+        if(bits[i] == 0) {						// If it hits a 0, increment to 1
+            bits[i] = 1;						// "Increment"
+            break;							// Exit the loop once we know we've incremented
         }
-        if(bits[i] == 1) {								// If it hits a 1, we need to keep looping and change to 0
+        if(bits[i] == 1) {						// If it hits a 1, we need to keep looping and change to 0
             bits[i] = 0;
         }
     }
-	return;												// Return (null)
+	return;								// Return (null)
 }
 
 /*-------------------------*/
@@ -67,13 +67,13 @@ void Increment(int8_t bits[8]) {
 void Unsigned2Bits(uint32_t n, int8_t bits[8]) {
     int i = 0;
     int r;
-    while(i < 8) {										// Using the repeated division method, loop through bit array
-        r = n % 2;										// Take mod of value n as remainders
-        n = n / 2;										// Then take integer value of n/2
-        bits[i] = r;									// Our bits (in reverse b0 to b7) will be filled by the remainders
-        i++;											// Increment i
+    while(i < 8) {							// Using the repeated division method, loop through bit array
+        r = n % 2;							// Take mod of value n as remainders
+        n = n / 2;							// Then take integer value of n/2
+        bits[i] = r;							// Our bits (in reverse b0 to b7) will be filled by the remainders
+        i++;								// Increment i
     }
-	return;												// Return (null)
+    return;								// Return (null)
 }
 
 /*-------------------------*/
---
+++ COEN/COEN20/README.md
@@ -0,0 +1,3 @@
+### COEN 20 - Introduction to Embedded Systems
+
+#####  Introduction to computer organization: CPU, registers, buses, memory, I/O interfaces. Number systems: arithmetic and information representation. Assembly language programming: addressing techniques, arithmetic and logic operations, branching and looping, stack operations, procedure calls, parameter passing, and interrupts. C language programming: pointers, memory management, stack frames, interrupt processing.
